85482e5455f0ceb7f2833f4cc63925f92bcb21af
Jens Fischer <jens.fischer@otto.de>
2022-05-09 16:16:58 +0200

    Lib version bumps

--- src/main/java/de/otto/dash/config/WebSecurityConfiguration.java
+++ src/main/java/de/otto/dash/config/WebSecurityConfiguration.java
@@ -1,14 +1,21 @@
 package de.otto.dash.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/**");
+public class WebSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
     }
 }
